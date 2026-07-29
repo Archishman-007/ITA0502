dfa6e6bba36bd26aa9879f902c254c55e84e9cf8
Archishman-007 <sunnyarchish@gmail.com>
2026-07-29 19:02:52 +0530

    Add files via upload

---
+++ Lab Experiments/LAB.EXP/ex10.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("butterfly.jpg")
+
+rows, cols = img.shape[:2]
+
+M = np.float32([[1, 0, 100],
+                [0, 1, 50]])
+
+translated = cv2.warpAffine(img, M, (cols, rows))
+
+cv2.imshow("Original", img)
+cv2.imshow("Translated", translated)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
